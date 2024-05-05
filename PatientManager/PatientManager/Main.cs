f5fbe0567e0246241b107e6816fde829dfa1eb46
Samuel Gossweiler <Caleb.anotnio88823@gmail.com>
2024-05-05 10:59:28 -0400

    Añadiendo read/write y ubicaciones del .json

--- PatientManager/PatientManager/Main.cs
+++ PatientManager/PatientManager/Main.cs
@@ -1,6 +1,5 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.IO;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace PatientManager
 {
@@ -11,26 +10,30 @@ namespace PatientManager
         public string CI { get; set; }
         public string BloodGroup { get; set; }
     }
-
     public class PatientManager
     {
-        private const string FilePath = "patients.json";
+        private readonly string _filePath;
+
+        public PatientManager(IConfiguration configuration)
+        {
+            _filePath = configuration["PatientFileSettings:FilePath"];
+        }
 
         public List<Patient> ReadPatientsFromFile()
         {
-            if (!File.Exists(FilePath))
+            if (!File.Exists(_filePath))
             {
                 return new List<Patient>();
             }
 
-            string json = File.ReadAllText(FilePath);
+            string json = File.ReadAllText(_filePath);
             return JsonConvert.DeserializeObject<List<Patient>>(json);
         }
 
         public void WritePatientsToFile(List<Patient> patients)
         {
             string json = JsonConvert.SerializeObject(patients);
-            File.WriteAllText(FilePath, json);
+            File.WriteAllText(_filePath, json);
         }
     }
 }
